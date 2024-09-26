8b6e774ff0a871923f14ec91b6767362f61883e6
siganushka <siganushka@gmail.com>
2024-09-26 14:09:10 +0800

    style: improving layout style

--- assets/app.js
+++ assets/app.js
@@ -9,5 +9,4 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
 
-
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
