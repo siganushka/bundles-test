adfe9cfa406007e6cf922e92b57c64fa91e54327
siganushka <siganushka@gmail.com>
2024-10-21 17:28:46 +0800

    refactor: remove ux turbo

--- config/bundles.php
+++ config/bundles.php
@@ -17,5 +17,4 @@ return [
     Siganushka\RegionBundle\SiganushkaRegionBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
-    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
