f2d81c830318843e2f04aff352a2616c113754a8
siganushka <siganushka@gmail.com>
2024-10-26 18:02:06 +0800

    refactor: add collection test

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -4,15 +4,75 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Siganushka\ProductBundle\Form\ProductOptionValueType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class IndexController extends AbstractController
 {
     #[Route('/')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        return $this->render('index/index.html.twig');
+        $data = [
+            'tags' => ['', '', 'baz'],
+            'subform' => [
+                ['foo' => 'a', 'bar' => false],
+                ['foo' => 'b', 'bar' => true],
+            ]
+        ];
+
+        $builder = $this->createFormBuilder($data, ['required' => false])
+            ->add('tags', CollectionType::class, [
+                'entry_type' => TextType::class,
+                'entry_options' => [
+                    'label' => false,
+                    'constraints' => new NotBlank(),
+                    'row_attr' => ['class' => 'bbb'],
+                ],
+                'add_button_options' => [
+                    'row_attr' => ['class' => 'ccc'],
+                ],
+                'delete_button_options' => [
+                    'row_attr' => ['class' => 'ddd'],
+                ],
+                'allow_add' => true,
+                'allow_delete' => true,
+                # 'disabled' => true,
+            ])
+            ->add('values', CollectionType::class, [
+                'entry_type' => ProductOptionValueType::class,
+                'entry_options' => [
+                    'label' => false,
+                    'constraints' => new NotBlank(),
+                    'row_attr' => ['class' => 'bbb'],
+                ],
+                'add_button_options' => [
+                    'row_attr' => ['class' => 'ccc'],
+                ],
+                'delete_button_options' => [
+                    'row_attr' => ['class' => 'ddd'],
+                ],
+                'allow_add' => true,
+                'allow_delete' => true,
+            ])
+            ->add('submit', SubmitType::class)
+        ;
+
+        $form = $builder->getForm();
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd(__METHOD__, $form->getData());
+        }
+
+        return $this->render('index/index.html.twig', [
+            'form' => $form,
+        ]);
     }
 }
