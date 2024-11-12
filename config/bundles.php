b72f99c9d3655ec9248ab8be2c5cd0938310d74b
siganushka <siganushka@gmail.com>
2024-11-12 16:04:31 +0800

    refactor: improving order intentory

--- config/bundles.php
+++ config/bundles.php
@@ -3,7 +3,6 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
@@ -16,5 +15,7 @@ return [
     Siganushka\OrderBundle\SiganushkaOrderBundle::class => ['all' => true],
     Siganushka\RegionBundle\SiganushkaRegionBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Siganushka\RequestTokenBundle\SiganushkaRequestTokenBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
