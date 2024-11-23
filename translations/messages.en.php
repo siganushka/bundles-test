fd4c5cc50c3b0204416ceaade82d1d060babb4ab
siganushka <siganushka@gmail.com>
2024-11-23 14:39:58 +0800

    style: improving show order page style

---
+++ translations/messages.en.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'order.adjustment.shipping_fee' => 'Shipping fee',
+    'order.adjustment.random_discount' => 'Random Discount',
+    'order.adjustment.coupon_discount' => 'Coupon Discount',
+];
---
+++ translations/messages.zh_CN.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'order.adjustment.shipping_fee' => '运费',
+    'order.adjustment.random_discount' => '随机立减',
+    'order.adjustment.coupon_discount' => '优惠券抵扣',
+];
