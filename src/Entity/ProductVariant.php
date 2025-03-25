22126a59b4644c1659125b6c7ea8fe66c194c7f3
siganushka <siganushka@gmail.com>
2025-03-25 15:58:25 +0800

    refactor: improving OrderItemSubjectInterface

--- src/Entity/ProductVariant.php
+++ src/Entity/ProductVariant.php
@@ -12,18 +12,4 @@ use Siganushka\ProductBundle\Entity\ProductVariant as BaseProductVariant;
 #[ORM\Entity(repositoryClass: ProductVariantRepository::class)]
 class ProductVariant extends BaseProductVariant implements OrderItemSubjectInterface
 {
-    public function getName(): ?string
-    {
-        $label = $this->getChoiceLabel();
-        if (!$this->product) {
-            return $label;
-        }
-
-        $productName = $this->product->getName();
-        if (\is_string($productName) && \is_string($label)) {
-            return \sprintf('%s【%s】', $productName, $label);
-        }
-
-        return $productName;
-    }
 }
