f161135aa2ea34704c5516cd621779b6d30be1ae
siganushka <siganushka@gmail.com>
2025-11-03 11:34:30 +0800

    refactor: remove unused code

--- translations/messages.zh_CN.php
+++ translations/messages.zh_CN.php
@@ -3,13 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'product.virtual' => '虚拟',
-    'product.weight' => '重量（kg）',
-    'product.volume' => '体积（cm³）',
-    'product.length' => '长（cm）',
-    'product.width' => '宽（cm）',
-    'product.height' => '高（cm）',
-
     'order.adjustment.shipping_fee' => '运费',
     'order.adjustment.random_discount' => '随机立减',
     'order.adjustment.coupon_discount' => '优惠券抵扣',
