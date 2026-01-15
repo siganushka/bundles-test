b02183a5466ba6a9b25e38b0a89d3a58083c04ba
siganushka <siganushka@gmail.com>
2026-01-15 17:11:11 +0800

    refactor: update deps

--- src/Entity/Test111.php
+++ src/Entity/Test111.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Siganushka\GenericBundle\Entity\Embeddable\DateRange;
 
 #[ORM\Entity]
 class Test111 extends TestInheritanceJoined
 {
     #[ORM\Column]
     private ?string $a = null;
+
+    #[ORM\Embedded(columnPrefix: false)]
+    private ?DateRange $range = null;
 
     public function getA(): ?string
     {
@@ -20,6 +24,18 @@ class Test111 extends TestInheritanceJoined
     public function setA(string $a): static
     {
         $this->a = $a;
+
+        return $this;
+    }
+
+    public function getRange(): ?DateRange
+    {
+        return $this->range;
+    }
+
+    public function setRange(DateRange $range): ?static
+    {
+        $this->range = $range;
 
         return $this;
     }
