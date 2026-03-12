3e02c4fa5150eb6dacb99c70416ea3215fe7c55b
siganushka <siganushka@gmail.com>
2026-03-12 16:05:02 +0800

    refactor: Crud Attribute

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,24 +4,14 @@ declare(strict_types=1);
 
 namespace App;
 
-use App\Attribute\Crud;
 use App\Doctrine\Type\BrickMoneyType;
 use Doctrine\DBAL\Types\Type;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\DependencyInjection\ChildDefinition;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
-
-    public function build(ContainerBuilder $container): void
-    {
-        $container->registerAttributeForAutoconfiguration(Crud::class, static function (ChildDefinition $definition): void {
-            $definition->addTag('app.crud');
-        });
-    }
 
     public function boot(): void
     {
