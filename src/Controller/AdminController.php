56b757f5c4676a2090bc3be96c9a938fb6952640
siganushka <siganushka@gmail.com>
2026-03-24 14:30:25 +0800

    refactor: update deps

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -4,22 +4,23 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Siganushka\UserBundle\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Core\User\InMemoryUser;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin/dashboard')]
-    public function dashboard(?Security $security): Response
+    public function dashboard(UserRepository $userRepository, ?Security $security): Response
     {
         $this->addFlash('success', 'Welcome to SiganushkaAdminBundle!');
 
         try {
             // The "symfony/security-bundle" is optional
-            $security && $security->login(new InMemoryUser('Siganushka', '123456'));
+            $user = $userRepository->findOneByIdentifier('siganushka');
+            $security && $user && $security->login($user);
         } catch (\Throwable $th) {
             dd($th);
         }
