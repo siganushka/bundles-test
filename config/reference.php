3da1188f8260affbc6320dcea28d2b55f9339117
siganushka <siganushka@gmail.com>
2026-06-15 11:09:58 +0800

    refactor: update deps

--- config/reference.php
+++ config/reference.php
@@ -1215,7 +1215,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  * @psalm-type SiganushkaMediaConfig = array{
  *     media_class?: scalar|Param|null, // Default: "Siganushka\\MediaBundle\\Entity\\Media"
  *     storage?: scalar|Param|null, // Default: "Siganushka\\MediaBundle\\Storage\\LocalStorage"
- *     naming?: scalar|Param|null, // This value defines the default file naming strategy (Available placeholders: yy/yyyy/m/mm/d/dd/timestamp/uniqid/random/random:{LENGTH}:{START}/rule/ext/original_name_with_ext). // Default: "[random:2]/[random:2:2]/[random:12:4].[ext]"
+ *     naming?: scalar|Param|null, // This value defines the default file naming strategy (Available placeholders: yy/yyyy/m/mm/d/dd/timestamp/random/random:{LENGTH}:{START}/rule/ext/original_name/original_ext). // Default: "[random:2]/[random:2:2]/[random:12:4].[ext]"
  *     rules?: list<array{ // Default: []
  *         constraint?: scalar|Param|null, // This value will be used for validation when uploading files. // Default: "Symfony\\Component\\Validator\\Constraints\\File"
  *         constraint_options?: array<string, mixed>,
@@ -1330,7 +1330,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *             servicename?: scalar|Param|null, // Overrules dbname parameter if given and used as SERVICE_NAME or SID connection parameter for Oracle depending on the service parameter.
  *             sessionMode?: scalar|Param|null, // The session mode to use for the oci8 driver
  *             server?: scalar|Param|null, // The name of a running database server to connect to for SQL Anywhere.
- *             default_dbname?: scalar|Param|null, // Override the default database (postgres) to connect to for PostgreSQL connexion.
+ *             default_dbname?: scalar|Param|null, // Override the default database (postgres) to connect to for PostgreSQL connection.
  *             sslmode?: scalar|Param|null, // Determines whether or with what priority a SSL TCP/IP connection will be negotiated with the server for PostgreSQL.
  *             sslrootcert?: scalar|Param|null, // The name of a file containing SSL certificate authority (CA) certificate(s). If the file exists, the server's certificate will be verified to be signed by one of these authorities.
  *             sslcert?: scalar|Param|null, // The path to the SSL client certificate file for PostgreSQL.
@@ -1381,7 +1381,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *                 servicename?: scalar|Param|null, // Overrules dbname parameter if given and used as SERVICE_NAME or SID connection parameter for Oracle depending on the service parameter.
  *                 sessionMode?: scalar|Param|null, // The session mode to use for the oci8 driver
  *                 server?: scalar|Param|null, // The name of a running database server to connect to for SQL Anywhere.
- *                 default_dbname?: scalar|Param|null, // Override the default database (postgres) to connect to for PostgreSQL connexion.
+ *                 default_dbname?: scalar|Param|null, // Override the default database (postgres) to connect to for PostgreSQL connection.
  *                 sslmode?: scalar|Param|null, // Determines whether or with what priority a SSL TCP/IP connection will be negotiated with the server for PostgreSQL.
  *                 sslrootcert?: scalar|Param|null, // The name of a file containing SSL certificate authority (CA) certificate(s). If the file exists, the server's certificate will be verified to be signed by one of these authorities.
  *                 sslcert?: scalar|Param|null, // The path to the SSL client certificate file for PostgreSQL.
@@ -1413,7 +1413,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *                 servicename?: scalar|Param|null, // Overrules dbname parameter if given and used as SERVICE_NAME or SID connection parameter for Oracle depending on the service parameter.
  *                 sessionMode?: scalar|Param|null, // The session mode to use for the oci8 driver
  *                 server?: scalar|Param|null, // The name of a running database server to connect to for SQL Anywhere.
- *                 default_dbname?: scalar|Param|null, // Override the default database (postgres) to connect to for PostgreSQL connexion.
+ *                 default_dbname?: scalar|Param|null, // Override the default database (postgres) to connect to for PostgreSQL connection.
  *                 sslmode?: scalar|Param|null, // Determines whether or with what priority a SSL TCP/IP connection will be negotiated with the server for PostgreSQL.
  *                 sslrootcert?: scalar|Param|null, // The name of a file containing SSL certificate authority (CA) certificate(s). If the file exists, the server's certificate will be verified to be signed by one of these authorities.
  *                 sslcert?: scalar|Param|null, // The path to the SSL client certificate file for PostgreSQL.
