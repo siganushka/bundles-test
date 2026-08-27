62461c9c72e473d0e8a1064994a2e79ae0d82027
siganushka <siganushka@gmail.com>
2026-08-27 14:35:51 +0800

    refactor: imporve user fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -20,15 +20,15 @@ class UserFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $user0 = $this->userRepository->createNew();
-        $user0->setIdentifier('siganushka');
+        $user0->setIdentifier(uniqid('siganushka.'));
         $user0->setPassword($this->passwordHasher->hashPassword($user0, '123456'));
 
         $user1 = $this->userRepository->createNew();
-        $user1->setIdentifier('zhangsan');
+        $user1->setIdentifier(uniqid('zhangsan.'));
         $user1->setPassword($this->passwordHasher->hashPassword($user1, '123456'));
 
         $user2 = $this->userRepository->createNew();
-        $user2->setIdentifier('lisi');
+        $user2->setIdentifier(uniqid('lisi.'));
         $user2->setPassword($this->passwordHasher->hashPassword($user2, '123456'));
         $user2->setEnabled(false);
 
